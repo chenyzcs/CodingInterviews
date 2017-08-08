21343919a18419802e4d3f213ed20d176e92385d
chenyzcs <chenyzcs@163.com>
2017-08-08 16:53:25 +0800

    add binary tree file

---
+++ utility/BinaryTree.h
@@ -0,0 +1,19 @@
+#ifndef BINARYTREE_H
+#define BINARYTREE_H
+
+#include <iostream>
+#include <vector>
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x = INF) : val(x), left(nullptr), right(nullptr) {}
+}
+
+class BINARYTREE_H
+{
+};
+
+#endif // BINARYTREE_H
