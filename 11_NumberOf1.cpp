6ec1b5c52d35b2b86b5d005674c313298a465e20
chenyzcs <chenyzcs@163.com>
2017-08-09 21:14:50 +0800

    figure out 11 number of 1

---
+++ 11_NumberOf1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <limits>
+#include <bitset>
+
+using namespace std;
+
+int NumberOf1(int n)
+{
+    int cnt = 0;
+    while (n)
+    {
+        cnt++;
+        n = n & (n - 1);
+    }
+    return cnt;
+}
+
+int bitsetSolution(int n)
+{
+    size_t sz = numeric_limits<unsigned int>::digits;
+    bitset<sz> b(n);
+    return b.count();
+}
+
+int main()
+{
+    cout << NumberOf1(10) << endl;
+    cout << bitsetSolution(10) << endl;
+    return 0;
+}
